c8beeda0e11e699b6cb8add0e03410f692795ee6
eggadigga <egga19@yahoo.com>
2024-03-06 15:09:49 -0500

    updated

--- real_options_vwap_signaling.py
+++ real_options_vwap_signaling.py
@@ -1,7 +1,7 @@
 #! python3
 
 '''
-name: real_trader_egg.py
+name: real_options_vwap_signaling.py
 description: trade bot placing random trades on egga's dime
 author: eggadigga
 '''
@@ -29,16 +29,23 @@ real_api_key = os.environ['apcarealkey']
 real_api_secret = os.environ['apcarealsecret']
 exchange = AlpacaReal(real_api_key, real_api_secret)
 
-def get_most_active_stocks(num_stocks, price_limit):
+def get_most_active_stocks(num_stocks, price_limit, type_):
  #### Get Most Active Stocks ####
  #### Limit number of stocks and specify stock price limit to trade
+    '''
+    :param type_ => call or put
+    '''
     symbols = []
     most_active = exchange.get_most_active_stocks_by_volume(num_stocks)['most_actives'] ## specify top returned. 100 max
     for sym in most_active:
         price = exchange.get_latest_stock_bar(sym['symbol'])['bar']['c']
         vwap = exchange.get_latest_stock_bar(sym['symbol'])['bar']['vw']
-        if int(float(price)) <= price_limit and price > vwap: ## limit to stocks under a specified price point and with price > vwap
-            symbols.append(sym['symbol'])
+        if type_ == 'call':
+            if int(float(price)) <= price_limit and price > vwap: ## use for call
+                symbols.append(sym['symbol'])
+        else:
+            if int(float(price)) <= price_limit and price < vwap: ## use for put
+                symbols.append(sym['symbol'])
     return symbols
     
 if __name__ == '__main__':
@@ -47,5 +54,12 @@ if __name__ == '__main__':
     print('\nEgga\'s stock trading bot is now running.')
     print('Author: eggadigga\n')
     print('$'*75)
-    symbols = get_most_active_stocks(num_stocks=100, price_limit=400)
+
+    if len(sys.argv) != 2:
+        raise Exception('Error: Missing argument "call" or "put"!!')
+    elif sys.argv[1] not in ['call', 'put']:
+        raise Exception('Error: Invalid argument. Accepted are "call" or "put" only!')
+    else:
+        option = sys.argv[1]
+    symbols = get_most_active_stocks(num_stocks=100, price_limit=400,type_=option)
     print(symbols)
